703058398fbfcb5f03a447e9bbe287f4e3358ad6
Nindaff <pisacanen@gmail.com>
2015-10-23 14:22:52 -0700

    initial

---
+++ README.md
@@ -0,0 +1,84 @@
+# Middleman Redis Store
+  [![Travis][travis-image]][travis-url]
+
+A Redis store implementation for middleman-proxy.
+
+## Installation
+$ npm install --save middleman-redis-store
+
+## Usage
+```js
+const Middleman = require('middleman-proxy');
+const RedisStore = require('middleman-redis-store');
+
+const proxy = new Middleman({
+  target: 'http://some.api.com',
+  store: new RedisStore()
+});
+```
+
+## Examples
+
+### Custom redis client
+```js
+const client = redis.createClient() // or ioRedis.createClient();
+const store = new RedisStore({
+  client: client
+});
+```
+
+### Expiration Policy
+```js
+const store = new RedisStore({
+  expire: 3600000 // milliseconds
+});
+
+// Because Redis will be managing expiration, its best to set `lru: false` and NOT
+// set `maxAge` in the Middleman options.
+const proxy = new Middleman({
+  lru: false,
+  store: store
+});
+```
+
+## API
+#### RedisStore([options])
+If no `client`, `url`, or `socket` are specified in `options`, they will be passed
+directly to `redis.createClient(options).`
+
+##### client
+Type: RedisClient (node-redis, or ioRedis)
+Default: `redis.createClient()`
+##### prefix
+Type: `string`
+Default: `"middlman"`
+
+Prefix all keys saved by the store.
+##### expire
+Type: `number`
+
+Number in **milliseconds** for time-to-live on all keys saved by RedisStore. By
+default no expiration policy is used.
+**NOTE** If you use the redis expiration policy, you will most likely want to disable
+LRU in Middleman.
+##### url
+Type: `string`
+
+Redis url.
+##### socket
+Type: `string`
+
+Redis socket.
+
+##### pass
+Type: `string`
+
+Password for redis authentication.
+
+##### db
+Type: `number`
+
+Select a redis database.
+
+[travis-image]: https://travis-ci.org/Nindaff/middleman-redis-store.svg?branch=master
+[travis-url]: https://travis-ci.org/Nindaff/middleman-redis-store
---
+++ test/redisStore_test.js
@@ -0,0 +1,85 @@
+var RedisStore = require('../');
+var redis = require('redis');
+var ioredis = require('ioredis');
+var should = require('should');
+
+function RedisServer() {
+
+}
+
+function testLifeCycle(store, done) {
+  done = done || false;
+  var test = store.get('not_set')
+    .then(function(val) {
+      should(val).equal(null);
+      return store.set('foo', 'bar');
+    })
+    .then(function(val) {
+      val.should.equal('bar');
+      return store.get('foo');
+    })
+    .then(function(val) {
+      val.should.equal('bar');
+      return store.set('foo', 'baz')
+    })
+    .then(function(val) {
+      val.should.equal('baz');
+      return store.get('foo');
+    })
+    .then(function(val) {
+      val.should.equal('baz');
+      return store.del('foo');
+    })
+    .then(function(val) {
+      val.should.equal(true);
+    });
+  if (!done) {
+    return test;
+  }
+  test
+    .then(function() {
+      done();
+    })
+    .catch(done);
+}
+
+describe('RedisStore', function() {
+  it('constructor should not require `new` keyword', function() {
+    RedisStore().should.be.instanceof(RedisStore);
+  });
+
+  it('defaults', function(done) {
+    var store = new RedisStore();
+    store.prefix.should.equal('middleman:');
+    store.client.should.be.instanceof(redis.RedisClient);
+    should(store.expire).equal(null);
+    store._expirationPolicy.should.equal(false);
+    testLifeCycle(store, done);
+  });
+
+  it('should use existing client (node-redis)', function(done) {
+    var client = redis.createClient();
+    var store = new RedisStore({client: client});
+    testLifeCycle(store, done);
+  });
+
+  it('should use existing client (ioredis)', function(done) {
+    var client = ioredis.createClient();
+    var store = new RedisStore({client: client});
+    testLifeCycle(store, done);
+  });
+
+  it('options', function(done) {
+    var store = new RedisStore({
+      port: 6379,
+      host: 'localhost',
+      prefix: 'foo',
+      expire: 1000
+    });
+
+    store.prefix.should.equal('foo');
+    store._expirationPolicy.should.equal(true);
+    store.expire.should.equal(1000);
+    testLifeCycle(store, done);
+  });
+});
